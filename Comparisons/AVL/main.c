105fe1d7da2807232177e077352b873d2d3d105e
Matheus Martins <matheusmbatista2009@gmail.com>
2022-06-19 12:37:01 -0300

    update

--- Comparisons/AVL/main.c
+++ Comparisons/AVL/main.c
@@ -11,8 +11,9 @@ int main() {
     double FreeT = 0;
 
     avl *arvAleatoria = criaArvore();
-    avl *arvOrdenada = criaArvore();
+    avl *arvOrdenada= criaArvore();
     avl *arvDescrescente = criaArvore();
+
     
     printf("Aleatorio\n");
     //N = 1000
@@ -32,13 +33,320 @@ int main() {
         SearchTime += (double)(fim - inicio) / CLOCKS_PER_SEC;
     }
     SearchTime = SearchTime / 30;
-    inicio = clock();
+    /*inicio = clock();
     esvaziaArvore(arvAleatoria, "data/1K.txt");
     fim = clock();
     FreeT = (double)(fim - inicio) / CLOCKS_PER_SEC;
-    rotLibera = getRotacoes(arvAleatoria) - rotCarrega;
+    rotLibera = getRotacoes(arvAleatoria) - rotCarrega;*/
+
+    registraDados(ChargeT, FreeT, SearchTime, "registro.txt", qtd, altura, type, rotCarrega, rotLibera);
+
+    /*N = 10000
+    SearchTime = 0;
+    rotCarrega = 0;
+    rotLibera = 0;
+    qtd = qtd*10;
+    inicio = clock();
+    carregaArvore(arvAleatoria, "data/10K.txt");
+    fim = clock();
+    ChargeT = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    rotCarrega = getRotacoes(arvAleatoria);
+    altura = getAltura(getRaiz(arvAleatoria));
+    //Busca
+    for(i = 0; i < 30; i++){
+        chave = rand() % 30000;
+        inicio = clock();
+        recuperaNo(arvAleatoria, chave);
+        fim = clock();
+        SearchTime += (double)(fim - inicio) / CLOCKS_PER_SEC;
+    }
+    SearchTime = SearchTime / 30;
+    inicio = clock();
+    esvaziaArvore(arvAleatoria, "data/10K.txt");
+    fim = clock();
+    FreeT = (double)(fim - inicio) / CLOCKS_PER_SEC;*/
+
+    //registraDados(ChargeT, FreeT, SearchTime, "registro.txt", qtd, altura, type, rotCarrega, rotLibera);
+
+    /*N = 100000
+    SearchTime = 0;
+    SearchTime = 0;
+    rotCarrega = 0;
+    rotLibera = 0;
+    qtd = qtd*10;
+    inicio = clock();
+    carregaArvore(arvAleatoria, "data/100K.txt");
+    fim = clock();
+    ChargeT = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    rotCarrega = getRotacoes(arvAleatoria);
+    altura = getAltura(getRaiz(arvAleatoria));
+    //Busca
+    for(i = 0; i < 30; i++){
+        chave = rand() % 30000;
+        inicio = clock();
+        recuperaNo(arvAleatoria, chave);
+        fim = clock();
+        SearchTime += (double)(fim - inicio) / CLOCKS_PER_SEC;
+    }
+    SearchTime = SearchTime / 30;
+    inicio = clock();
+    esvaziaArvore(arvAleatoria, "data/100K.txt");
+    fim = clock();
+    FreeT = (double)(fim - inicio) / CLOCKS_PER_SEC;*/
+
+    //registraDados(ChargeT, FreeT, SearchTime, "registro.txt", qtd, altura, type, rotCarrega, rotLibera);
+
+    /*N = 1000000
+    SearchTime = 0;
+    SearchTime = 0;
+    rotCarrega = 0;
+    rotLibera = 0;
+    qtd = qtd*10;
+    inicio = clock();
+    carregaArvore(arvAleatoria, "data/1M.txt");
+    fim = clock();
+    ChargeT = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    rotCarrega = getRotacoes(arvAleatoria);
+    altura = getAltura(getRaiz(arvAleatoria));
+    //Busca
+    for(i = 0; i < 30; i++){
+        chave = rand() % 30000;
+        inicio = clock();
+        recuperaNo(arvAleatoria, chave);
+        fim = clock();
+        SearchTime += (double)(fim - inicio) / CLOCKS_PER_SEC;
+    }
+    SearchTime = SearchTime / 30;
+    inicio = clock();
+    esvaziaArvore(arvAleatoria, "data/1M.txt");
+    fim = clock();
+    FreeT = (double)(fim - inicio) / CLOCKS_PER_SEC;*/
+
+    //registraDados(ChargeT, FreeT, SearchTime, "registro.txt", qtd, altura, type, rotCarrega, rotLibera);
+
+    SearchTime = 0;
+    SearchTime = 0;
+    rotCarrega = 0;
+    rotLibera = 0;
+    //-------------------------------------Crescente------------------------------------
+    printf("Crescente\n");
+    type = 2;
+    qtd = 1000;
+    inicio = clock();
+    carregaArvore(arvOrdenada, "data/1K_Ord.txt");
+    fim = clock();
+    ChargeT = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    rotCarrega = getRotacoes(arvOrdenada);
+    altura = getAltura(getRaiz(arvOrdenada));
+    //Busca
+    for(i = 0; i < 30; i++){
+        chave = rand() % 30000;
+        inicio = clock();
+        recuperaNo(arvOrdenada, chave);
+        fim = clock();
+        SearchTime += (double)(fim - inicio) / CLOCKS_PER_SEC;
+    }
+    SearchTime = SearchTime / 30;
+    /*inicio = clock();
+    esvaziaArvore(arvOrdenada, "data/1K_Ord.txt");
+    fim = clock();
+    FreeT = (double)(fim - inicio) / CLOCKS_PER_SEC;*/
 
     registraDados(ChargeT, FreeT, SearchTime, "registro.txt", qtd, altura, type, rotCarrega, rotLibera);
     
+    //N = 10000
+    SearchTime = 0;
+    rotCarrega = 0;
+    rotLibera = 0;
+    qtd = qtd*10;
+    inicio = clock();
+    carregaArvore(arvOrdenada, "data/10K_Ord.txt");
+    fim = clock();
+    ChargeT = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    rotCarrega = getRotacoes(arvOrdenada);
+    altura = getAltura(getRaiz(arvOrdenada));
+    //Busca
+    for(i = 0; i < 30; i++){
+        chave = rand() % 30000;
+        inicio = clock();
+        recuperaNo(arvOrdenada, chave);
+        fim = clock();
+        SearchTime += (double)(fim - inicio) / CLOCKS_PER_SEC;
+    }
+    SearchTime = SearchTime / 30;
+    /*inicio = clock();
+    esvaziaArvore(arvOrdenada, "data/10K_Ord.txt");
+    fim = clock();
+    FreeT = (double)(fim - inicio) / CLOCKS_PER_SEC;*/
+
+    registraDados(ChargeT, FreeT, SearchTime, "registro.txt", qtd, altura, type, rotCarrega, rotLibera);
+
+    //N = 100000
+    SearchTime = 0;
+    rotCarrega = 0;
+    rotLibera = 0;
+    qtd = qtd*10;
+    inicio = clock();
+    carregaArvore(arvOrdenada, "data/100K_Ord.txt");
+    fim = clock();
+    ChargeT = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    rotCarrega = getRotacoes(arvOrdenada);
+    altura = getAltura(getRaiz(arvOrdenada));
+    //Busca
+    for(i = 0; i < 30; i++){
+        chave = rand() % 30000;
+        inicio = clock();
+        recuperaNo(arvOrdenada, chave);
+        fim = clock();
+        SearchTime += (double)(fim - inicio) / CLOCKS_PER_SEC;
+    }
+    SearchTime = SearchTime / 30;
+    /*inicio = clock();
+    esvaziaArvore(arvOrdenada, "data/100K_Ord.txt");
+    fim = clock();
+    FreeT = (double)(fim - inicio) / CLOCKS_PER_SEC;*/
+
+    registraDados(ChargeT, FreeT, SearchTime, "registro.txt", qtd, altura, type, rotCarrega, rotLibera);
+
+    /*N = 1000000
+    SearchTime = 0;
+    rotCarrega = 0;
+    rotLibera = 0;
+    qtd = qtd*10;
+    inicio = clock();
+    carregaArvore(arvOrdenada, "data/1M_Ord.txt");
+    fim = clock();
+    ChargeT = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    rotCarrega = getRotacoes(arvOrdenada);
+    altura = getAltura(getRaiz(arvOrdenada));
+    //Busca
+    for(i = 0; i < 30; i++){
+        chave = rand() % 30000;
+        inicio = clock();
+        recuperaNo(arvOrdenada, chave);
+        fim = clock();
+        SearchTime += (double)(fim - inicio) / CLOCKS_PER_SEC;
+    }
+    SearchTime = SearchTime / 30;
+    inicio = clock();
+    esvaziaArvore(arvOrdenada, "data/1M_Ord.txt");
+    fim = clock();
+    FreeT = (double)(fim - inicio) / CLOCKS_PER_SEC;*/
+
+    //registraDados(ChargeT, FreeT, SearchTime, "registro.txt", qtd, altura, type, rotCarrega, rotLibera);
+    
+    SearchTime = 0;
+    rotCarrega = 0;
+    rotLibera = 0;
+    //-------------------------------------Decrescente------------------------------------
+    printf("Decrescente\n");
+    type = 3;
+    qtd = 1000;
+    inicio = clock();
+    carregaArvore(arvDescrescente, "data/1K_Reverse.txt");
+    fim = clock();
+    ChargeT = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    rotCarrega = getRotacoes(arvDescrescente);
+    altura = getAltura(getRaiz(arvDescrescente));
+    //Busca
+    for(i = 0; i < 30; i++){
+        chave = rand() % 30000;
+        inicio = clock();
+        recuperaNo(arvDescrescente, chave);
+        fim = clock();
+        SearchTime += (double)(fim - inicio) / CLOCKS_PER_SEC;
+    }
+    SearchTime = SearchTime / 30;
+    /*inicio = clock();
+    esvaziaArvore(arvDescrescente, "data/1K_Reverse.txt");
+    fim = clock();
+    FreeT = (double)(fim - inicio) / CLOCKS_PER_SEC;*/
+
+    registraDados(ChargeT, FreeT, SearchTime, "registro.txt", qtd, altura, type, rotCarrega, rotLibera);
+    
+    //N = 10000
+    SearchTime = 0;
+    rotCarrega = 0;
+    rotLibera = 0;
+    qtd = qtd*10;
+    inicio = clock();
+    carregaArvore(arvDescrescente, "data/10K_Reverse.txt");
+    fim = clock();
+    ChargeT = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    rotCarrega = getRotacoes(arvDescrescente);
+    altura = getAltura(getRaiz(arvDescrescente));
+    //Busca
+    for(i = 0; i < 30; i++){
+        chave = rand() % 30000;
+        inicio = clock();
+        recuperaNo(arvDescrescente, chave);
+        fim = clock();
+        SearchTime += (double)(fim - inicio) / CLOCKS_PER_SEC;
+    }
+    SearchTime = SearchTime / 30;
+    /*inicio = clock();
+    esvaziaArvore(arvDescrescente, "data/10K_Reverse.txt");
+    fim = clock();
+    FreeT = (double)(fim - inicio) / CLOCKS_PER_SEC;*/
+
+    registraDados(ChargeT, FreeT, SearchTime, "registro.txt", qtd, altura, type, rotCarrega, rotLibera);
+
+    //N = 100000
+    SearchTime = 0;
+    rotCarrega = 0;
+    rotLibera = 0;
+    qtd = qtd*10;
+    inicio = clock();
+    carregaArvore(arvDescrescente, "data/100K_Reverse.txt");
+    fim = clock();
+    ChargeT = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    rotCarrega = getRotacoes(arvDescrescente);
+    altura = getAltura(getRaiz(arvDescrescente));
+    //Busca
+    for(i = 0; i < 30; i++){
+        chave = rand() % 30000;
+        inicio = clock();
+        recuperaNo(arvDescrescente, chave);
+        fim = clock();
+        SearchTime += (double)(fim - inicio) / CLOCKS_PER_SEC;
+    }
+    SearchTime = SearchTime / 30;
+    /*inicio = clock();
+    esvaziaArvore(arvDescrescente, "data/100K_Reverse.txt");
+    fim = clock();
+    FreeT = (double)(fim - inicio) / CLOCKS_PER_SEC;*/
+
+    registraDados(ChargeT, FreeT, SearchTime, "registro.txt", qtd, altura, type, rotCarrega, rotLibera);
+
+    /*N = 1000000
+    SearchTime = 0;
+    rotCarrega = 0;
+    rotLibera = 0;
+    qtd = qtd*10;
+    inicio = clock();
+    carregaArvore(arvDescrescente, "data/1M_Reverse.txt");
+    fim = clock();
+    ChargeT = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    rotCarrega = getRotacoes(arvDescrescente);
+    altura = getAltura(getRaiz(arvDescrescente));
+    //Busca
+    for(i = 0; i < 30; i++){
+        chave = rand() % 30000;
+        inicio = clock();
+        recuperaNo(arvDescrescente, chave);
+        fim = clock();
+        SearchTime += (double)(fim - inicio) / CLOCKS_PER_SEC;
+    }
+    SearchTime = SearchTime / 30;
+    inicio = clock();
+    esvaziaArvore(arvDescrescente, "data/1M_Reverse.txt");
+    fim = clock();
+    FreeT = (double)(fim - inicio) / CLOCKS_PER_SEC;*/
+
+    //registraDados(ChargeT, FreeT, SearchTime, "registro.txt", qtd, altura, type, rotCarrega, rotLibera);
+
+    free(arvAleatoria);
+    free(arvDescrescente);
+    free(arvOrdenada);
     return 0;
 }
