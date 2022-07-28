3140b3e07c18a4bad0082d86fc172a7bac96fbfb
Matheus Martins <matheusmbatista2009@gmail.com>
2022-07-28 16:38:05 -0300

    updates

---
+++ RB/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "rb.h"
+
+int main() {
+    int chave;
+    char nomeArquivo[100]; // = "entrada.txt";
+    rb *novaArvore = criaArvore();
+
+    scanf("%s", nomeArquivo);
+
+    carregaArvore(novaArvore, nomeArquivo);
+    percorre(getRaiz(novaArvore));
+    printf("\n");
+    printf("\nAltura da arvore: %d.\n", getAltura(getRaiz(novaArvore)));
+
+    scanf("%d", &chave);
+    removeNo(novaArvore, chave);
+    percorre(getRaiz(novaArvore));
+    printf("\n");
+
+    scanf("%d", &chave);
+    removeNo(novaArvore, chave);
+    percorre(getRaiz(novaArvore));
+    printf("\n");
+
+    scanf("%d", &chave);
+    removeNo(novaArvore, chave);
+    percorre(getRaiz(novaArvore));
+
+    printf("\n");
+
+    percorre(getRaiz(novaArvore));
+    printf("\nAltura da arvore: %d.\n", getAltura(getRaiz(novaArvore)));
+
+    free(novaArvore);
+}
